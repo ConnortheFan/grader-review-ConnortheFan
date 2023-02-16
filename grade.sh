10c580df37b9abc95315017b5ab9adcaa937e406
Connor Fan <ConnortheFan@gmail.com>
2023-02-16 14:32:28 -0800

    Moved all files to same directory

--- grade.sh
+++ grade.sh
@@ -1,20 +1,25 @@
 CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
 
-if [[ $# < 2]]
-then 
-    echo "Missing arguments!"
-    exit 1
-fi
+# if [[ $# -lt 1]]
+# then 
+#     echo "Missing arguments!"
+#     exit 1
+# fi
 
 rm -rf student-submission
 git clone $1 student-submission
 echo 'Finished cloning'
 
-if [[-e ListExamples.java]]
+if [[ -e student-submission/ListExamples.java ]]
 then 
     echo "ListExamples.java found"
 else
     echo "File ListExamples.java not found"
     exit 1
 fi
+
+cp -r lib student-submission/
+cp TestListExamples.java student-submission/
+cd student-submission/
+
 
