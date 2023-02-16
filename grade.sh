496f77925ee3cba9b48248726226e6799e63a5c4
Connor Fan <ConnortheFan@gmail.com>
2023-02-16 15:31:16 -0800

    Run tests and display color
    Co-authored-by: Alon Lahav <alonlahav123@users.noreply.github.com>

--- grade.sh
+++ grade.sh
@@ -1,4 +1,5 @@
-CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
+#CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
+CPATH=".;lib/hamcrest-core-1.3.jar;lib/junit-4.13.2.jar"
 
 # if [[ $# -lt 1]]
 # then 
@@ -23,12 +24,33 @@ cp TestListExamples.java student-submission/
 cd student-submission/
 
 set +e
-javac -cp CPATH *.java
+javac -cp $CPATH *.java
 
-if [[ $? != 0 ]]
-then 
+if [[ $? -eq 0 ]]
+then
+    echo "Compile succeeded"
+else
     echo "Failed to compile" 
     exit 1
 fi
+
+java -cp $CPATH org.junit.runner.JUnitCore TestListExamples > test-results.txt
+
+#grep --color -w "OK\|FAILURES!!!" test-results.txt 
+#grep --color "Failures:" test-results.txt 
+
+GREEN='\033[1;32m'
+
+if grep -q "OK" test-results.txt; then
+    echo -e "${GREEN}Test is all good, very nice 10/10"
+fi
+
+RED='\033[1;31m'
+
+if grep --color "FAILURES!!!" test-results.txt; then
+    grep --color "Failures:" test-results.txt
+    echo -e "${RED}Test failed. Try harder next time ;("
+fi
+
 
 
