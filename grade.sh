bfa143455b1a6042737a4dcee1309d6f6ac3214e
Connor Fan <ConnortheFan@gmail.com>
2023-02-16 14:22:27 -0800

    Check for correct file submitted
    Co-authored-by: Alon Lahav <alonlahav123@users.noreply.github.com>

--- grade.sh
+++ grade.sh
@@ -1,5 +1,20 @@
 CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
 
+if [[ $# < 2]]
+then 
+    echo "Missing arguments!"
+    exit 1
+fi
+
 rm -rf student-submission
 git clone $1 student-submission
 echo 'Finished cloning'
+
+if [[-e ListExamples.java]]
+then 
+    echo "ListExamples.java found"
+else
+    echo "File ListExamples.java not found"
+    exit 1
+fi
+
